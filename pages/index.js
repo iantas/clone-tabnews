5476d34121bbbb7bfa2cec242680a98cf371316e
iantas <104787731+iantas@users.noreply.github.com>
2025-10-29 00:02:55 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home () {
-    return <h1>O curso.dev, realmente é diferente.</h1>
+function Home() {
+  return <h1>O curso.dev, realmente é diferente.</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
